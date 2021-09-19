9788930eb925e6f8e77d64a3052dcbb8580352f8
Elder Carvalho <eld.carv@gmail.com>
2021-09-19 12:35:14 -0300

    feat: :alembic: experimental code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import Card, { CardProps } from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import styled from 'styled-components';
 
-import generateMessage, { Message } from './Api';
+import { Grid } from '@material-ui/core';
+import generateMessage, { Message, Priority } from './Api';
+
+const Container = styled.main({
+  maxWidth: '1000px',
+  margin: '0 auto',
+});
+
+const CardButton = styled(Button)({
+  textTransform: 'none',
+});
+
+interface ColoredCardProps {
+  backgroundColor: string;
+}
 
 const App: React.FC<{}> = () => {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -13,11 +32,52 @@ const App: React.FC<{}> = () => {
   }, [setMessages]);
 
   return (
-    <div>
-      {messages?.map?.((msg) => (
-        <div key={msg?.message}>{msg?.message}</div>
-      ))}
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs>
+          {messages
+            ?.filter((message) => message.priority === Priority.Error)
+            .map?.((msg) => (
+              <Card key={msg?.message}>
+                <CardContent>{msg?.message}</CardContent>
+                <CardActions>
+                  <CardButton variant="text" size="small">
+                    Clear
+                  </CardButton>
+                </CardActions>
+              </Card>
+            ))}
+        </Grid>
+        <Grid item>
+          {messages
+            ?.filter((message) => message.priority === Priority.Info)
+            .map?.((msg) => (
+              <Card key={msg?.message}>
+                <CardContent>{msg?.message}</CardContent>
+                <CardActions>
+                  <CardButton variant="text" size="small">
+                    Clear
+                  </CardButton>
+                </CardActions>
+              </Card>
+            ))}
+        </Grid>
+        <Grid item>
+          {messages
+            ?.filter((message) => message.priority === Priority.Warn)
+            .map?.((msg) => (
+              <Card key={msg?.message}>
+                <CardContent>{msg?.message}</CardContent>
+                <CardActions>
+                  <CardButton variant="text" size="small">
+                    Clear
+                  </CardButton>
+                </CardActions>
+              </Card>
+            ))}
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
